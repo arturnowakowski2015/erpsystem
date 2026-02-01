8bfdffad7ec09be9c68f63936785a0307979b6f4
Man Rami <manramirami02@gmail.com>
2026-02-01 09:59:47 +0530

    Delete src directory
    Signed-off-by:  Man Rami <manramirami02@gmail.com>

--- src/pages/sale/index.ts
+++
@@ -1,8 +0,0 @@
-// Sale module exports
-export { default as SalesOrderList } from './SalesOrderList';
-export { default as CustomerInvoiceList } from './CustomerInvoiceList';
-export { default as InvoicePaymentList } from './InvoicePaymentList';
-export { default as SalesOrderDetail } from './SalesOrderDetail';
-export { default as CustomerInvoiceDetail } from './CustomerInvoiceDetail';
-export { default as SalesOrderForm } from './SalesOrderForm';
-export { default as CustomerInvoiceForm } from './CustomerInvoiceForm';
